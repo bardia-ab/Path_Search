287bbcc55e4eb268a14f2a88604d825eefd595bb
Bardia Babaei <bardia.babaei@postgrad.manchester.ac.uk>
2023-09-21 11:20:17 +0100

    update node

--- resources/node.py
+++ resources/node.py
@@ -11,7 +11,7 @@ class Node:
         return self.name
 
     def __eq__(self, other):
-        return self.name == other.name
+        return type(self) == type(other) and self.name == other.name
 
     def __hash__(self):
         return hash(self.name)
